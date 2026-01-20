dcc8cab30a5e05ee7f490fdbe8df3106ce345845
Korakod <ice9393th@gmail.com>
2026-01-20 20:50:05 +0700

    API order

--- src/app/api/orders/route.js
+++ src/app/api/orders/route.js
@@ -1,6 +1,43 @@
 import { NextResponse } from "next/server";
 import mongoose from "mongoose";
-import connectDB from "@/app/lib/mongoose"
-import Order from "@/app/Models/Order"
-import Product from "@/app/Models/Product"
+import connectDB from "@/app/lib/mongoose";
+import Order from "@/app/Models/Order";
+import Product from "@/app/Models/Product";
 
+//{
+//     "UserId": "1234",
+//     "items" :[{
+//       "productId": "65prod001",
+//       "qty": 2,
+//       "discount": 0
+//     }],
+//     "OptionPayment":"cash"
+// }//
+
+export async function POST(req) {
+  await connectDB();
+
+  // เริ่มการบันทึกทุกอย่างจากนี้
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    let body = await req.json();
+    const { UserId, items, OptionPayment } = body;
+    if (!UserId || !items || !items.length === 0) {
+      throw new Error("Invalid order data");
+    }
+
+   let orderItem = [];
+    let Total = 0;
+
+    // for (const item in items) {
+    //     orderItem.push(item)
+    // }
+    // console.log(orderItem)
+
+    return NextResponse.json(body, { status: 200 });
+  } catch (error) {
+    console.log("Something Error :", error);
+  }
+}
