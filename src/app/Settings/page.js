7647cddd30470a5755d84afd0162d93f19dff581
Korakod <ice9393th@gmail.com>
2026-01-27 20:31:49 +0700

    fix : ui navbar color  and add mock page

---
+++ src/app/Settings/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='flex min-h-screen w-screen items-center justify-center bg-gray-500 font-sans'>Settings</div>
+  )
+}
--- src/app/component/navbar/navbar.jsx
+++ src/app/component/navbar/navbar.jsx
@@ -3,25 +3,37 @@ import React from 'react'
 import { useRouter } from 'next/navigation'
 function NavbarUI() {
     let route = useRouter()
-    function Home(params) {
-    route.push("/login")
+    function Home(num) {
+   route.push("/")
+    }
+      function Tables(num) {
+   route.push("/Tables")
+    }
+      function Order(num) {
+   route.push("/Order")
+    }
+      function Products(num) {
+   route.push("/Products")
+    }
+      function Settings(num) {
+   route.push("/Settings")
     }
   return (
          <nav className=' bg-gray-300 p-4 grid w-[250px] justify-center text-center'>
           
-            <header className='mt-20'>  <h1 className='text-2xl'>POS</h1></header>
+            <header className='mt-20 hover:cursor-pointer' onClick={Home} >  <h1 className='text-2xl'>POS</h1></header>
  
              <section>
 <ul className='mt-20'>
     <li onClick={Home} >Home</li>
-    <li>Tables</li>
-    <li>Order</li>
-    <li>Products</li>
-    <li>Settings</li>
+    <li onClick={Tables}>Tables</li>
+    <li onClick={Order}>Order</li>
+    <li onClick={Products}>Products</li>
+    <li onClick={Settings}>Settings</li>
 </ul>
             </section>
             <section className=' mt-40'>
-                <h1>Logout</h1>
+                <h1 className='font-bold bg-red-500 px-2 py-2 hover:cursor-pointer hover:bg-red-400 text-white'>Logout</h1>
                 </section>
 
          </nav>
