cd41d963fea7f5571d4d9821248ae4d405c9d854
Korakod <ice9393th@gmail.com>
2026-01-31 20:58:54 +0700

    alert/notice ui

--- src/app/component/CardShow.jsx
+++ src/app/component/CardShow.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {Card} from "@heroui/react";
 
-export default function CardShow({Title,Sale,style}) {
+export default function CardShow({Title,Text,style}) {
   return (
-     <Card className={`w-[400px] ml-2  mr-2 text-center ${style} h-[100px] `}  variant="default">
+     <Card className={`w-[400px] ml-2 rounded-2xl  mr-2 text-center ${style} h-[100px] `}  variant="default">
            <Card.Header>
              <Card.Title className='text-3xl font-bold'>{Title}</Card.Title>
            </Card.Header>
            <Card.Content >
-             <p className='text-2xl'>{Sale}</p>
+             <p className='text-2xl'>{Text}</p>
            </Card.Content>
          </Card>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -4,15 +4,29 @@ import Table from "./component/Table";
 export default function Home() {
   return (
     <main className="min-h-screen w-screen  bg-gray-300 font-sans ">
-
-       <header className=" flex  p-4 justify-around  border-b-4 border-white h-[125px] ">
-        <CardShow Title={"Today's Sales"} Sale={"฿ 4,520"} style={""}/>
-        <CardShow Title={"Orders Today"} Sale={" 38  "} style={""}/>
-        <CardShow Title={" Low Stock"} Sale={" 5 items"} style={""}/>
-        <CardShow Title={"New Account To day"} Sale={"11"} style={""}/>
+      <header>Test</header>
+       <section>
+         <header className=" flex  px-4 justify-around  mt-10 border-b-4 border-white h-[125px] ">
+        <CardShow Title={"Today's Sales"} Text={"฿ 4,520"} style={""}/>
+        <CardShow Title={"Orders Today"} Text={" 38  "} style={""}/>
+        <CardShow Title={" Low Stock"} Text={" 5 items"} style={""}/>
+        <CardShow Title={"New Account To day"} Text={"11"} style={""}/>
        </header>
-       <section className="border-b-4 mt-4  border-white"><Table/></section>
-       
+       </section>
+       <section className="border-b-4 mt-2  border-white grid">
+        <header className=" p-4 text-3xl font-bold">
+          Recent Orders
+       </header><Table/>
+      </section>
+       <section className="grid border-b-4 border-white h-[300px]">
+        <header className="text-3xl p-4 font-bold mt-2">Alert / Notice</header>
+   <div className="flex m">
+     <CardShow Title={"สินค้าใกล้หมด"} Text={"4"} style={""}/>
+    <CardShow Title={"Order ถูกยกเลิก"} Text={"1"} style={""}/>
+    <CardShow Title={"ลูกค้าแนะนำร้านวันนี้"} Text={2 +" คน"} style={""}/>
+    <CardShow Title={"ร้องเรียน"} Text={1 +" คน"} style={""}/>
+   </div>
+       </section>
 
     </main>
   );
