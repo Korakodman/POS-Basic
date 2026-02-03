69e92213bfa9121c03b8581b2f3755e77362ed1e
Korakod <ice9393th@gmail.com>
2026-02-03 20:56:06 +0700

    pos basic ui

--- src/app/POS/page.js
+++ src/app/POS/page.js
@@ -22,22 +22,64 @@ export default function page() {
       {/* <------- Section ------> */}
       <section className="grid border-t-4 border-white ">
         {/* <----- Header list ----- > */}
-                <section className=" h-[500px]">
+        <section className=" h-[500px]">
           <div>
-            <header className="p-4 bg-gray-300 flex justify-between">
-              <h1 className="p-2">สินค้า</h1>
+            <header className="p-4 bg-gray-300 flex justify-between font-bold">
+              <h1 className="p-2 ">สินค้า</h1>
               <div className="flex p-2 w-60  justify-between ">
                 <h1>ราคา</h1>
                 <h1>จำนวน</h1>
               </div>
             </header>
-            <section className=" bg-gray-100 p-2 h-[400]"> text</section>
+            <section className=" bg-gray-100 p-2 h-[400]">
+              <div className="p-2 flex justify-between">
+                <h1>pencil</h1>
+                <div className="flex w-60 p-2 justify-between">
+                  <h1>20</h1>
+                  <h1>2</h1>
+                </div>
+              </div>
+               <div className="p-2 flex justify-between">
+                <h1>เลย์</h1>
+                <div className="flex w-60 p-2 justify-between">
+                  <h1>22</h1>
+                  <h1>2</h1>
+                </div>
+              </div>
+               <div className="p-2 flex justify-between">
+                <h1>text</h1>
+                <div className="flex w-60 p-2 justify-between">
+                  <h1>210</h1>
+                  <h1>2</h1>
+                </div>
+              </div>
+            </section>
           </div>
+          <section className=" flex justify-between font-bold bg-amber-200 h-20 items-center">
+            <div>
+              <h1 className="p-2 "> รวม</h1>
+            </div>
+            <div className="flex p-2 justify-between w-60">
+              <h1>{252} บาท</h1>
+              <h1>{3} รายการ </h1>
+              <h1>{6} ชิ้น</h1>
+            </div>
+          </section>
         </section>
-          {/* <----- Header list ----- > */}
+        {/* <----- Header list ----- > */}
         <section className="flex p-2 h-[300] items-center mx-56 justify-center ">
-          <section><ButtonUI text={"Cash"} style={"bg-yellow-500 px-20 py-6 text-xl mr-20"}/></section>
-          <section><ButtonUI text={"Payment"} style={"bg-blue-500 px-20 py-6 text-xl"}/></section>
+          <section>
+            <ButtonUI
+              text={"Cash"}
+              style={"bg-yellow-500 px-20 py-6 text-xl mr-20"}
+            />
+          </section>
+          <section>
+            <ButtonUI
+              text={"Payment"}
+              style={"bg-blue-500 px-20 py-6 text-xl"}
+            />
+          </section>
         </section>
       </section>
       {/* <------- Section ------> */}
